01fb915a17d4974387a4c039a9e8a189c04dae2c
Amanda Ferreira <amandaf1604@gmail.com>
2022-01-31 15:29:41 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -2,19 +2,19 @@ package com.educandoweb.course.entities;
 
 import java.util.Objects;
 
-import javax.persistence.Embedded;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.educandoweb.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem {
 
 	 @EmbeddedId
-	 private OrderItemPK id;
+	 private OrderItemPK id = new OrderItemPK(	);
 	 
 	 private Integer quantity;
 	 private Double price;
@@ -30,6 +30,7 @@ public class OrderItem {
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
